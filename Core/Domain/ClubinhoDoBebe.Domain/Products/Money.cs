c31ff5b3cc38881883f573b3b4b48cacc22fdafa
Isaac de Sousa <isaacslima@gmail.com>
2023-04-21 10:23:13 -0300

    adding Product entity

---
+++ Core/Domain/ClubinhoDoBebe.Domain/Products/Money.cs
@@ -0,0 +1,3 @@
+﻿namespace ClubinhoDoBebe.Domain.Products;
+
+public record Money(string Currency, decimal Amount);
---
+++ Core/Domain/ClubinhoDoBebe.Domain/Products/Sku.cs
@@ -0,0 +1,23 @@
+﻿namespace ClubinhoDoBebe.Domain.Products;
+
+//Stock Keeping Unit
+public record Sku
+{
+
+    private const int DefaultLength = 15;
+    private Sku(string value) => Value = value;
+
+    public string Value { get; init; }
+
+    public static Sku? Create(string value)
+    {
+        if (string.IsNullOrEmpty(value)) return null;
+
+        if (value.Length != DefaultLength)
+        {
+            return null;
+        }
+
+        return new Sku(value);
+    }
+}
