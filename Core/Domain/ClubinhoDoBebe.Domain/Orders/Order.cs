f06397ccd5ed56e00c076bd2ac914920378de222
Isaac de Sousa <isaacslima@gmail.com>
2023-04-21 10:37:32 -0300

    adding order entity

---
+++ Core/Domain/ClubinhoDoBebe.Domain/Orders/Order.cs
@@ -0,0 +1,35 @@
+﻿using ClubinhoDoBebe.Domain.Customers;
+using ClubinhoDoBebe.Domain.Products;
+
+namespace ClubinhoDoBebe.Domain.Orders;
+
+public class Order
+{
+    private readonly HashSet<LineItem> _lineItems = new();
+
+    private Order()
+    { 
+    }
+
+    public Guid Id { get; private set; }
+
+    public Guid CustomerId { get; private set; }
+
+    public static Order Create(Customer customer)
+    {
+        var order = new Order
+        {
+            Id = Guid.NewGuid(),
+            CustomerId = customer.Id
+        };
+
+        return order;
+    }
+
+    public void Add(Product product)
+    {
+        var lineItem = new LineItem(Guid.NewGuid(), Id, product.Id, product.Price);
+
+        _lineItems.Add(lineItem);
+    }
+}
