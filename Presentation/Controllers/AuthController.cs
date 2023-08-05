b91e7040ad1d97885ec83aed864ed27f5c76a190
Isaac de Sousa <isaacslima@gmail.com>
2023-08-05 00:47:13 -0300

    removing incorrect project and adding application and infrastructure

---
+++ Presentation/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClubinhoDoBebe.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
