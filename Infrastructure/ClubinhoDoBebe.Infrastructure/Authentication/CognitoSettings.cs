8ab7193c65cb67e7985b996799e79cdc80b47bde
Isaac de Sousa <isaacslima@gmail.com>
2023-08-05 02:20:32 -0300

    adding CognitoSettings to project

---
+++ Infrastructure/ClubinhoDoBebe.Infrastructure/Authentication/CognitoSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ClubinhoDoBebe.Infrastructure.Authentication;
+
+[ExcludeFromCodeCoverage]
+public class CognitoSettings
+{
+    public const string SectionName = "CognitoSettings";
+    public string Region { get; set; } = null!;
+    public string UserPoolId { get; set; } = null!;
+    public string ClientId { get; set; } = null!;
+    public string ClientSecret { get; set; } = null!;
+}
